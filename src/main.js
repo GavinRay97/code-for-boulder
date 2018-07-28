c9c119fc82954b4317d63c56a693c793fa75d85d
Unknown <ray.gavin97@gmail.com>
2018-07-28 15:43:05 -0600

    add google map

--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(BootstrapVue)
+
+import 'vue-googlemaps/dist/vue-googlemaps.css'
+import VueGoogleMaps from 'vue-googlemaps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    // Google API key
+    apiKey: 'AIzaSyAEXEXjH196dIau12Z2lq45a-aBy_1-2d8',
+    // Enable more Google Maps libraries here
+    libraries: ['places'],
+    // Use new renderer
+    useBetaRenderer: false,
+  },
+})
+
 Vue.config.productionTip = false
 
 new Vue({
